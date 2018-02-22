c16ec29771bae7cc474a67e786de78b52edf3093
Christan Grant <christangrant@gmail.com>
2018-02-22 15:28:46 -0600

    Script to run all the data files

---
+++ RUN_ALL.sh
@@ -0,0 +1,48 @@
+#! /usr/bin/bash
+
+dbfiles=(
+/home/solaimani/data/DISK1_CORENLP_RUN1.db_petrarch_done.db
+/home/solaimani/data/DISK1_CORENLP_RUN2.db_petrarch_done.db
+/home/solaimani/data/DISK2_CORENLP_V1_RUN1.db_petrarch_done.db
+/home/solaimani/data/DISK2_CORENLP_V2_RUN1.db_petrarch.db
+/home/solaimani/data/DISK2_CORENLP_V3_RUN1.db_petrarch.db
+/home/solaimani/data/DISK2_CORENLP_V4_RUN1.db_petrarch.db
+/home/solaimani/data/DISK2_KINGSTON_RUN1.db_petrarch.db
+/home/solaimani/data/DISK2_KINGSTON_RUN2.db_petrarch.db
+/home/solaimani/data/DISK2_PORTLAND_RUN3.db_petrarch.db
+/home/solaimani/data/DISK2_PORTLAND_VM_RUN1.db_petrarch.db
+/home/solaimani/data/DISK2_PORTLAND_VM_RUN2.db_petrarch.db
+/home/solaimani/data/DISK2_VM5_RUN1.db_petrarch.db
+/home/solaimani/data/DISK2_VM6_RUN1.db_petrarch.db
+/home/solaimani/data/DISK2_VM7_RUN1.db_petrarch.db
+/home/solaimani/data/DISK2_VM8_RUN1.db_petrarch.db
+/home/solaimani/data/gpel12_run1.db_petrarch.db
+/home/solaimani/data/gpel12_run2.db_petrarch.db
+/home/solaimani/data/gpel13_run1.db_petrarch.db
+/home/solaimani/data/gpel13_run2.db_petrarch.db
+/home/solaimani/data/gpel8_run1.db_petrarch.db
+/home/solaimani/data/gpel8_run2.db_petrarch.db
+/home/solaimani/data/gpel8_run3.db_petrarch.db
+/home/solaimani/data/gpel9_run2.db_petrarch.db
+/home/solaimani/data/KINGSTON_DISK2_CORENLP_FINAL1.db_petrarch.db
+/home/solaimani/data/KINGSTON_DISK2_CORENLP_FINAL_2.db_petrarch.db
+/home/solaimani/data/KINGSTON_DISK2_CORENLP_FINAL.db_petrarch.db
+)
+
+
+myrun () {
+	local item=$1
+	local fname=`basename $item`
+	local outfile="outfiles/"$fname".json"
+	#echo $fname
+	#echo $outfile
+	#ls -l $item
+	nice -n19 sqlite3 $item "select json_object('doc_id', doc_id, 'phrases', phrases, 'mongo_id', mongo_id, 'date', date) from petrarch_table" | parallel --no-notice --jobs +0 --pipe "python3 birdcage.py --data '-' >> $outfile"
+}
+
+for item in ${dbfiles[@]};
+do
+	myrun $item &
+done
+
+# nice -n10 sqlite3 ~solaimani/data/DISK1_CORENLP_RUN1.db_petrarch_done.db "select json_object('doc_id', doc_id, 'phrases', phrases, 'mongo_id', mongo_id, 'date', date) from petrarch_table limit 5" | parallel --no-notice --sshloginfile myservers --workdir ...  --pipe python3 birdcage.py --data '-'
