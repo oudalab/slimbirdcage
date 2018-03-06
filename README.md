1a945ccba301de39a0f7f5bffc2e10938b0e0382
Christan Grant <christangrant@gmail.com>
2018-03-06 14:01:28 -0600

    Updated to run on Xsede

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # slimbirdcage
 A slimer reimplementation of the openventdata/birdcage code
+
+# Dependencies
+
+GNU Parallel
+sqlite3 # (with Json support)
+python3.6 # or greater
+pip3 # apt install python3-pip
+
+
+Be sure to add a myservers file
+
+    ```echo ":" > myservers```
+
+pip3 install --upgrade pip3
+pip3 instal --user -r requirements.txt
+
+# Additional
+
+sudo apt install speedtest-cli iperf
+sudo apt install jq
---
+++ RUN_ONE.sh
@@ -0,0 +1,23 @@
+#! /usr/bin/bash
+
+dbfiles=(
+/vol_c/data/processed_data/DISK1_CORENLP_RUN1.db_petrarch_done.db
+)
+
+
+myrun () {
+	local item=$1
+	local fname=`basename $item`
+	local outfile="outfiles/"$fname".json"
+	#echo $fname
+	#echo $outfile
+	#ls -l $item
+	sqlite3 $item "select json_object('doc_id', doc_id, 'phrases', phrases, 'mongo_id', mongo_id, 'date', date) from petrarch_table" | parallel --no-notice --jobs +0 --pipe "python3 birdcage.py --data '-' >> $outfile"
+}
+
+for item in ${dbfiles[@]};
+do
+	myrun $item &
+done
+
+# nice -n10 sqlite3 ~solaimani/data/DISK1_CORENLP_RUN1.db_petrarch_done.db "select json_object('doc_id', doc_id, 'phrases', phrases, 'mongo_id', mongo_id, 'date', date) from petrarch_table limit 5" | parallel --no-notice --sshloginfile myservers --workdir ...  --pipe python3 birdcage.py --data '-'
